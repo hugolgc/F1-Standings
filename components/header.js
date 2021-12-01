c0f0cf929f20cbd1e494087ffec8f46d78fed61f
hugolgc <lagache.hugo@hotmail.fr>
2021-12-01 13:47:27 +0100

    Publicité

--- components/header.js
+++ components/header.js
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center">
       <div className="h-24 w-full md:w-96 mx-6 md:mx-0 flex bg-gray-800">
-        <p className="m-auto text-xl font-semibold">Google Ads</p>
+        <p className="m-auto text-xl font-semibold">Insertion publicitaire</p>
       </div>
     </header>
   )
